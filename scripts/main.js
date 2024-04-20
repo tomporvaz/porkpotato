706a44bac9da25ab9a35d689ccd86d8433a5d3c0
Tom <tomporvaz@gmail.com>
2024-04-20 14:54:45 -0400

    Made some progress installing slick slideshow and styling first slide.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,25 @@
 $(document).ready(function () {
-  $(".slideshow").slick( );
+  $(".slideshow").slick({
+    accessibility: true,
+    adaptiveHeight: true,
+    autoplay: true
+  });
 });
+
+//Animations
+let buildBtn = document.getElementById("build-btn");
+
+buildBtn.addEventListener("click", function () {
+  buildBtn.classList.add("animate__tada");
+});
+
+buildBtn.addEventListener("animationend", () => {
+  buildBtn.classList.remove("animate__tada");
+  console.log(buildBtn.classList);
+});
+
+// $('#build-btn').click(function (e) {
+//   let buildBtn = $(this)
+//   buildBtn.addClass('animate__rollOut');
+
+// });
